37d6bf83c45dad626eb44f98662773cfd26d6561
P1zza-L1cht <haruki21corder@gmail.com>
2021-05-23 10:43:30 +0900

    [finish]chapter2,3

---
+++ chapter2/app.js
@@ -0,0 +1,34 @@
+// 条件分岐
+// var orange = 100;
+// var apple = 10;
+
+// if(orange < apple){
+//   alert('みかんの値段がリンゴより安い');
+// } else if(orange == apple){
+//   alert('みかんとリンゴが同じ値段');
+// } else{
+//   alert('オレンジの値段がリンゴより高い');
+// }
+
+// 繰り返し処理
+// var max = 100;
+// var num = 1;
+// var count = 0;
+
+// while(num < max){
+//   num = num * 2;
+//   count = count + 1;
+// }
+
+// alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+
+var i;
+var num = 0;
+
+for(i = 1; i < 11; i++){
+  num = num + i;
+}
+
+alert('1から10まで足し算した結果は' + num + 'です');
+
+
